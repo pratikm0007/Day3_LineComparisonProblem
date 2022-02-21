4c39e0bd0d7c0a372637ecbccb790e0b53bdd325
pratikm0007 <pratikpmondal07@gmail.com>
2022-02-21 21:18:15 +0530

    Adding Compare Two Lines file

---
+++ UC_3CompareTwoLines.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LineComparisonProblem
+{
+    public class UC_3CompareTwoLines
+    {
+        public static void CompareTwoLineForGrtEqlLessThan()
+        {
+            UC_2CheckEquality.CheckEqualityOfTwoLines();
+
+            if (UC_1CalculateTheLength.lengthOfLine1 > UC_2CheckEquality.lengthOfLine2)
+            {
+                Console.WriteLine($"Length of line1 {UC_1CalculateTheLength.lengthOfLine1} is greater than Length of line2 {UC_2CheckEquality.lengthOfLine2}");
+            }
+            else if (UC_1CalculateTheLength.lengthOfLine1 == UC_2CheckEquality.lengthOfLine2)
+            {
+                Console.WriteLine($"Length of line1 {UC_1CalculateTheLength.lengthOfLine1} is equal to Length of line2 {UC_2CheckEquality.lengthOfLine2}");
+            }
+            else
+            {
+                Console.WriteLine($"Length of line1 {UC_1CalculateTheLength.lengthOfLine1} is less than Length of line2 {UC_2CheckEquality.lengthOfLine2}");
+            }
+        }
+    }
+}
