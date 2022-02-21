69497ba157d20bfbfa237f68664437ce548b4da2
pratikm0007 <pratikpmondal07@gmail.com>
2022-02-21 21:01:37 +0530

    Adding Program.cs file

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LineComparisonProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Co-ordinates value for the Length of Line");
+            UC_1CalculateTheLength.LenghtBtwnTwoPoint();
+
+            Console.WriteLine("Enter Co-ordinates value to Check Equality of two Lines");
+            UC_2CheckEquality.CheckEqualityOfTwoLines();
+
+            Console.WriteLine("Enter Co-ordinates value to Check Comparison of two Lines");
+            UC_3CompareTwoLines.CompareTwoLineForGrtEqlLessThan();
+
+        }
+    }
+
+}
